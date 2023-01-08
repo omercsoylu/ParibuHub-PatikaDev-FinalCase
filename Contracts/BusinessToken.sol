d337328cae4971ec0f944fb00d5b577883049309
omercsoylu <omercsoylu@gmail.com>
2023-01-08 19:09:33 +0300

    BusinessToken ERC20 Token Contract

---
+++ Contracts/BusinessToken.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BusinessToken is ERC20 {
+    mapping(address => bool) private owners;
+
+    modifier onlyOwners() {
+        require(owners[msg.sender] == true, "You're not one of owners.");
+        _;
+    }
+
+    constructor() ERC20("BusinessToken", "BST") {
+        owners[msg.sender] = true;
+    }
+
+    function addOwner(address _ownerAddress) external onlyOwners {
+        owners[_ownerAddress] = true;
+    }
+
+    function removeOwner(address _removeAddress) external onlyOwners {
+        delete owners[_removeAddress];
+    }
+
+    function mintToken(address _toAddress, uint256 _amount)
+        external
+        onlyOwners
+    {
+        _mint(_toAddress, _amount);
+    }
+
+
+}
