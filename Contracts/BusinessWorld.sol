65d69afd859d84d2e6da439f9aeda132ced68e14
omercsoylu <omercsoylu@gmail.com>
2023-01-13 18:12:57 +0300

    BusinessWorld - v0.2.7 - Final checks

--- Contracts/BusinessWorld.sol
+++ Contracts/BusinessWorld.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // @title BusinessCard NFT
 // @author @github/omercsoylu
